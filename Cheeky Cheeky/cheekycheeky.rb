4b1f2e8e28ecf14a4b061ab0014dc87af3c800c2
sebastian chande <chande@cs.fsu.edu>
2014-08-16 18:51:05 -0400

    Comments
    Added comments to file.

--- Cheeky Cheeky/cheekycheeky.rb
+++ Cheeky Cheeky/cheekycheeky.rb
@@ -4,10 +4,12 @@ ARGV.each do |file|
     if (line.length > 1)
         input = line
         output = ""
-
+        
+        #find index of middle of input string.
         midIndex = (input.length) / 2
         begIndex = 0
 
+        #move indexes until they're pointing at the same characters
         while midIndex != input.length
           if (input[midIndex] == input[begIndex])
             midIndex += 1
@@ -17,6 +19,7 @@ ARGV.each do |file|
           end
         end
 
+        #at this point, begIndex is the first character of the output
         while output.length < 8
           output += input[begIndex..(input.length - 1)]
         end
